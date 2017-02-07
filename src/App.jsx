c90b7a2b2a4692871b735c772b27aab1be3bfaa2
ericajlittle <ericalittle123@gmail.com>
2017-02-07 17:03:14 +0000

    started chatty app moved html into react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx'
+
 class App extends Component {
   render() {
+    console.log("Rendering <App/>");
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>Hello React :)</h1>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+
+        <MessageList />
+
+        <ChatBar />
+
+      </div>
     );
   }
 }
