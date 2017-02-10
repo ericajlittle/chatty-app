d94fcedce4c41ca428a4e4572c6a5a8851fc43a4
ericajlittle <ericalittle123@gmail.com>
2017-02-10 02:37:23 +0000

    created notifications for user change and client connection

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -3,41 +3,71 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const WebSocket = require('ws');
-
 const PORT = 8080;
-
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
-
-// Create the WebSockets server
 const wss = new SocketServer({ server });
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+var countConnection = 0;
+
+
+
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  countConnection++;
+  let message = {
+    type: 'counter',
+    countConnection: countConnection
+  }
+  broadcast(message);
 
-ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', function incoming(message) {
+    console.log('recenived: %s', message);
     message = JSON.parse(message);
-    broadcast(message);
+
+    switch(message.type) {
+      case "postMessage":
+        message.type = "incomingMessage";
+        console.log(message);
+        broadcast(message);
+        break;
+
+      case "postNotification":
+        message.type = "incomingNotification";
+        console.log(message);
+        broadcast(message);
+        break;
+
+      default:
+        console.error("bad message type", message.type);
+    }
+
   });
 
-function broadcast(data) {
-  wss.clients.forEach(function each(ws) {
-    if (ws.readyState === WebSocket.OPEN) {
-      newData = JSON.stringify(data);
-      ws.send(newData);
-    }
-  });
-};
+  function broadcast(data) {
+    wss.clients.forEach(function each(ws) {
+      if (ws.readyState === WebSocket.OPEN) {
+        newData = JSON.stringify(data);
+        ws.send(newData);
+        // console.log('newData: ', newData);
+      }
+    });
+  };
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected')
+    countConnection--
+    let message = {
+    type: 'counter',
+    countConnection: countConnection
+  }
+  broadcast(message);
+    console.log("client minus: ", countConnection);
+
+  });
 });
 
 
